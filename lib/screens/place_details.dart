b237b41ac68d943ec020fc47a4a08a99d6e96e21
snadi043 <snadi043@uottawa.ca>
2025-01-25 11:09:14 -0500

    Displaying the address and the image in the place details screen.

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -6,6 +6,12 @@ class PlaceDetailsScreen extends StatelessWidget {
 
   final Place places;
 
+  String get locationImage {
+    final lat = places.pickLocation.latitude;
+    final lng = places.pickLocation.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyBoMMjTdC54L0rctvSz2ZpJq99y3KFOg94';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,32 @@ class PlaceDetailsScreen extends StatelessWidget {
               width: double.infinity,
               fit: BoxFit.cover,
             ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: CircleAvatar(
+                radius: 70,
+                backgroundImage: NetworkImage(locationImage),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 16,
+              ),
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.transparent, Colors.black54],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+              child: Text(
+                places.pickLocation.address,
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            )
           ],
         ));
   }
