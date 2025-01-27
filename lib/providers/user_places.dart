712e38a2b517a4afc86196ae8a2d71f94ddecac0
snadi043 <snadi043@uottawa.ca>
2025-01-27 13:09:52 -0500

    Utilized all the packages and created a path to store the data locally on the devices irrespective of the operating systems deleting the data by default.

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:native_device_features/models/place.dart';
 
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as localpath;
+
 //StateNotifier is the riverpod provided generic type and in this case since we
 //expect places of the type Place defined in the model Place which is being used here
 // as the generic data type.
@@ -16,8 +19,16 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 // newPlace takes the newly adding place based on the Place model and then it gets
 // added to the previous exisiting state by doing object destrucutring.
 
-  void addPlaces(String title, File image, PickLocation location) {
-    final newPlace = Place(title: title, image: image, pickLocation: location);
+  void addPlaces(String title, File image, PickLocation location) async {
+    final Directory appDir = await syspaths
+        .getApplicationDocumentsDirectory(); // Using the installed path provider package to access the document directory methos which helps to store data locally.
+    final filename = localpath.basename(image
+        .path); // using the paths package to construct the local file names for the data which we are tying to save in the devices locally.
+    final imagePath = await image.copy(
+        '${appDir.path}/$filename'); // This is the final file directory to save the images locally on the device and it is passed to image property on the provider.
+
+    final newPlace =
+        Place(title: title, image: imagePath, pickLocation: location);
     state = [newPlace, ...state];
   }
 }
